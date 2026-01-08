234e6adbd9ee59dbf7b4eacd72932245aa398b42
FurtiAdi <furtiadi@gmail.com>
2026-01-08 09:50:21 +0100

    Added folders

--- L9/Project_two/AppTwo/views.py
+++ L9/Project_two/AppTwo/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-  return HttpResponse("<em>My Second App </em>")
+  my_dict = {'insert_me': "Hello I am from view.py!"}
+  return render(request, 'index.html', context=my_dict)
 
 
