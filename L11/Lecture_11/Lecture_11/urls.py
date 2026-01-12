c5b4e861aa14341dd20675602c855a6fbcda99ef
FurtiAdi <furtiadi@gmail.com>
2026-01-12 09:49:38 +0100

    Update urls

--- L11/Lecture_11/Lecture_11/urls.py
+++ L11/Lecture_11/Lecture_11/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import include
+from App import views
 
 urlpatterns = [
+    url(r'^App/', include('App.urls')),
     url(r'^admin/', admin.site.urls),
 ]
