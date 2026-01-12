bac9d0b6a2b300d5adf24595e752eae73dbed6a6
FurtiAdi <furtiadi@gmail.com>
2026-01-12 09:50:29 +0100

    Update with dict

--- L11/Lecture_11/App/views.py
+++ L11/Lecture_11/App/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from App.models import Topic, WebPage, AccessRecord
 
 # Create your views here.
+def index(request):
+  Webpages_list = AccessRecord.objects.order_by('date')
+  date_dict = {'access_records': Webpages_list}
+  my_dict = {
+        'insert_me': "Hello I am from views.py!"
+    }
+  return render(request, 'index.html', context=my_dict)
